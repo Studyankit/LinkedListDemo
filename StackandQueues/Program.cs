1f3bac7b4b6c83af7275a827dc786cd7a40f9a9f
StudyAnkit <ankitghosh2018@gmail.com>
2022-02-21 19:59:29 +0530

    Using peek and pop to remove data and check data

--- StackandQueues/Program.cs
+++ StackandQueues/Program.cs
@@ -5,7 +5,9 @@ StackandQueue linkedListStack = new StackandQueue();
 linkedListStack.Push(70);
 linkedListStack.Push(30);
 linkedListStack.Push(56);
-
+linkedListStack.Peek();
+linkedListStack.Pop();
+linkedListStack.IsEmpty();
 
 
 
